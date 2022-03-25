94b8639ac760b6fed9a265ec5750ff76e4da712e
chriskyong22 <55605854+chriskyong22@users.noreply.github.com>
2022-03-24 20:45:02 -0400

    init model

--- src/Services/API.ts
+++ src/Services/API.ts
@@ -1,5 +1,8 @@
 
 const BACKEND_ENDPOINT = "https://api.hatchways.io/assessment/students";
-export const fetchData = () => {
 
+export const fetchData = async (): Promise<any> => {
+    let response = await fetch(BACKEND_ENDPOINT);
+    let data = response.json();
+    return data;
 }
